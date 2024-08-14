223df3f7470221a2bfd2e73c38b829cea3dfcbae
AdithyaSean <adithyasean@gmail.com>
2024-08-14 23:38:11 +0530

    deleted:    .gitignore 	modified:   app.py 	new file:   yolov8s.pt

--- app.py
+++
@@ -1,5 +0,0 @@
-from pixellib.torchbackend.instance import instanceSegmentation
-
-ins = instanceSegmentation()
-ins.load_model("pointrend_resnet50.pkl")
-ins.process_video("input.mp4", show_bboxes=True, frames_per_second=3, output_video_name="output.mp4")
