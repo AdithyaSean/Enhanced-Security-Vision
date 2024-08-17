bae780c2eda02c46276cae32038d832d70e43910
AdithyaSean <adithyasean@gmail.com>
2024-08-17 15:45:40 +0530

    feat: Upscale face images for better quality and detail

--- src/segment.py
+++ src/segment.py
@@ -4,6 +4,7 @@ from enhance import enhance_image
 from night_vision import apply_night_vision
 
 def detect_faces(image_path, intensity):
+    upscale_factor = 2
     intensity_threshold = 50
     image = cv2.imread(image_path)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -18,9 +19,11 @@ def detect_faces(image_path, intensity):
         for (x, y, w, h) in faces:
             face = image[y:y+h, x:x+w]
 
+            upscaled_face = cv2.resize(face, (w * upscale_factor, h * upscale_factor), interpolation=cv2.INTER_LINEAR)
+
             # Path for saving the enhanced face image
             face_image_path = os.path.splitext(image_path)[0].replace('original', 'segmented') + '_face.jpg'
-            cv2.imwrite(face_image_path, face)
+            cv2.imwrite(face_image_path, upscaled_face)
 
             # Enhance the cropped face image and save it
             enhanced_face_path = face_image_path.replace('_face.jpg', '_enhanced_face.jpg')
